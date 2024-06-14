07175e46aad479962900ea1446d268aeb4942cf9
Goddess172007 <bak01072007@gmail.com>
2024-06-14 04:26:24 -0700

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,11 +5,11 @@ class Config(object):
     OWNER_ID = "7147738922"
     sudo_users = "7147738922", "6574393060"
     GROUP_ID = -1002029788751
-    TOKEN = ""
-    mongo_url = ""
+    TOKEN = "7197840791:AAHR-OqWB2L3cuhPxClWCHOFKi1ndaigTjg"
+    mongo_url = "mongodb+srv://DaxxMusic6:TeamDaxx1@cluster0.ln5y16q.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
+    SUPPORT_CHAT = "-1002192923367"
+    UPDATE_CHAT = "-1002194284395"
     BOT_USERNAME = "@godess_soulland_bot"
     CHARA_CHANNEL_ID = "-1002163390646"
     api_id = 26626068
