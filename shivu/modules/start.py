572d5259f8524163c96daa99f986160dd603ba3f
Shiva4648 <shiva.roll.no.75@gmail.com>
2024-06-29 01:03:46 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -72,11 +72,11 @@ async def button(update: Update, context: CallbackContext) -> None:
         help_text = """
     ***Help Section:***
     
-***/guess: To Guess character (only works in group)***
+***/seize: To Guess character (only works in group)***
 ***/fav: Add Your fav***
 ***/trade : To trade Characters***
 ***/gift: Give any Character from Your Collection to another user.. (only works in groups)***
-***/collection: To see Your Collection***
+***/seizes: To see Your Collection***
 ***/topgroups : See Top Groups.. Ppl Guesses Most in that Groups***
 ***/top: Too See Top Users***
 ***/ctop : Your ChatTop***
