82e7bb8662b22c165f41fd8ac47de6fd1bff15e6
Shiva4648 <shiva.roll.no.75@gmail.com>
2024-06-30 08:09:44 +0530

    Update upload.py

--- shivu/modules/upload.py
+++ shivu/modules/upload.py
@@ -12,7 +12,7 @@ img_url character-name anime-name rarity-number
 
 use rarity number accordingly rarity Map
 
-rarity_map = 1 (⚪️ Common), 2 (🟣 Rare) , 3 (🟡 Legendary), 4 (🟢 Medium)"""
+rarity_map = 1 (🟤 Bronze card), 2 (⚪ Silver card) , 3 (🟡 Gold card), 4 (💎 Daimond card), 5 (💮 Elite card), 6 (🎗 Master card)"""
 
 
 
@@ -48,11 +48,11 @@ async def upload(update: Update, context: CallbackContext) -> None:
             await update.message.reply_text('Invalid URL.')
             return
 
-        rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium"}
+        rarity_map = {1: "🟤 Bronze card", 2: "⚪ Silver card", 3: "🟡 Gold card", 4: "💎 Daimond card", 5: "💮 Elite card", 6: "🎗 Master card"}
         try:
             rarity = rarity_map[int(args[3])]
         except KeyError:
-            await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, 4, or 5.')
+            await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, 4, 5 or 6.')
             return
 
         id = str(await get_next_sequence_number('character_id')).zfill(2)
@@ -132,11 +132,11 @@ async def update(update: Update, context: CallbackContext) -> None:
         if args[1] in ['name', 'anime']:
             new_value = args[2].replace('-', ' ').title()
         elif args[1] == 'rarity':
-            rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Special edition"}
+            rarity_map = {1: "🟤 Bronze card", 2: "⚪ Silver card", 3: "🟡 Gold card", 4: "💎 Daimond card", 5: "💮 Elite card", 6: "🎗 Master card"}
             try:
                 new_value = rarity_map[int(args[2])]
             except KeyError:
-                await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, 4, or 5.')
+                await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, 4, 5 or 6.')
                 return
         else:
             new_value = args[2]
