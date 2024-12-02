0fc0b079c4af4aebfce3d714e61332427c34814d
surajdeveloper1 <surajmanikpuri601@gmail.com>
2024-12-03 04:02:58 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -8,12 +8,12 @@ class Config(object):
     TOKEN = "7589023976:AAFerTqtgkPEhmNsAU_5XOFtBaTcd7g-3EE"
     mongo_url = "mongodb+srv://surajmanikpuri601:surajgod112@cluster0.bmdh2.mongodb.net/
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
+    SUPPORT_CHAT = "ntpcraj"
+    UPDATE_CHAT = "ntpcdjk"
     BOT_USERNAME = "Collect_Em_AllBot"
     CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    api_id = 29830137
+    api_hash = "4fdb6b13530915e7b513ee4318f109b9"
 
     
 class Production(Config):
