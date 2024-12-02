d7b3097507417da5b12e2e771a4910807fe5df87
surajdeveloper1 <surajmanikpuri601@gmail.com>
2024-12-03 04:36:38 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -34,7 +34,7 @@ OWNER_ID = Config.OWNER_ID
 application = Application.builder().token(TOKEN).build()
 shivuu = Client("Shivu", api_id, api_hash, bot_token=TOKEN)
 lol = AsyncIOMotorClient(mongo_url)
-db = lol['Character_catcher']
+db = lol['random']
 collection = db['anime_characters_lol']
 user_totals_collection = db['user_totals_lmaoooo']
 user_collection = db["user_collection_lmaoooo"]
