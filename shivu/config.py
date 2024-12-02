b883ecfc7b7164000dcff093b3c26c2bd06e8602
surajdeveloper1 <surajmanikpuri601@gmail.com>
2024-12-03 03:56:52 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,11 +2,11 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
+    OWNER_ID = "6087651372"
+    sudo_users = "6845325416", "7667987606"
     GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    TOKEN = "7589023976:AAFerTqtgkPEhmNsAU_5XOFtBaTcd7g-3EE"
+    mongo_url = "mongodb+srv://surajmanikpuri601:surajgod112@cluster0.bmdh2.mongodb.net/
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Collect_em_support"
     UPDATE_CHAT = "Collect_em_support"
