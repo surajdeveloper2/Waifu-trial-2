cbe4bee5f7ffe0d6d48ecb26fc7bac4c5e202246
surajdeveloper1 <surajmanikpuri601@gmail.com>
2024-12-03 22:11:49 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -7,7 +7,7 @@ class Config(object):
     GROUP_ID = -1002133191051
     TOKEN = "7589023976:AAFerTqtgkPEhmNsAU_5XOFtBaTcd7g-3EE"
     mongo_url = "mongodb+srv://surajgod112:surajgod113@cluster0.v52wo.mongodb.net/"
-    PHOTO_URL = ["https://graph.org/file/87c05126a35a66d1d1d37-6f7a824e7306b10bd6.jpg", "https://graph.org/file/40d3df88f6d827ff48af5-9e72ee221223e291ed.jpg"]
+    PHOTO_URL = ["https://graph.org/file/87c05126a35a66d1d1d37-6f7a824e7306b10bd6.jpg", "https://envs.sh/CPY.mp4"]
     SUPPORT_CHAT = "ntpcraj"
     UPDATE_CHAT = "ntpcdjk"
     BOT_USERNAME = "Waifugrls17bot"
