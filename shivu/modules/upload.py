28dd0b8c0e26dd52e661c39376a0a19c05a27a20
surajdeveloper1 <surajmanikpuri601@gmail.com>
2024-12-03 05:46:43 +0530

    Update upload.py

--- shivu/modules/upload.py
+++ shivu/modules/upload.py
@@ -12,7 +12,7 @@ img_url character-name anime-name rarity-number
 
 use rarity number accordingly rarity Map
 
-rarity_map = 1 (⚪ Common), 2 (🟣 Normal) , 3 (🔵Medium), 4 (🟡 Legendary), 5 (💮 Mythical), 6 (🔮Limited Edition), 7 (🫧Premium), 8 (🎃Halloween), 9(💞Valentine), 10(🌲Christmas), 11(🐰Bunny), 13(🫦Sexy), 14(🍑Ultimate), 15(❤‍🔥Heaven)"""
+rarity_map = 1 (⚪ Common), 2 (🟣 Normal) , 3 (🔵Medium), 4 (🟡 Legendary), 5 (💮 Mythical), 6 (🔮Limited Edition), 7 (🫧Premium), 8 (🎃Halloween), 9(💞Valentine), 10(🌲Christmas), 11(🐰Bunny), 13(🫦Sexy), 14(🍑Ultimate), 15(🥵Heaven)"""
 
 
 
@@ -49,11 +49,11 @@ async def upload(update: Update, context: CallbackContext) -> None:
             await update.message.reply_text('Invalid URL.')
             return
 
-        rarity_map = {1: ""⚪ Common", 2: "🟣 Normal", 3: "🔵 Medium", 4: "🟡 Legendary", 5: "💮Mythical", 6: "🔮Limited Edition", 7: "🫧Premium", 8: "🎃Halloween", 9: "💞Valentine", 10: "🌲Christmas", 11: "🐰Bunny", 12: "🫦Sexy", 13: "🍑Ultimate", 14: "❤‍🔥Heaven"}
+        rarity_map = {1: ""⚪ Common", 2: "🟣 Normal", 3: "🔵 Medium", 4: "🟡 Legendary", 5: "💮Mythical", 6: "🔮Limited Edition", 7: "🫧Premium", 8: "🎃Halloween", 9: "💞Valentine", 10: "🌲Christmas", 11: "🐰Bunny", 12: "🫦Sexy", 13: "🍑Ultimate", 14: "🥵Heaven"}
         try:
             rarity = rarity_map[int(args[3])]
         except KeyError:
-            await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, 4, or 5.')
+            await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14.')
             return
 
         id = str(await get_next_sequence_number('character_id')).zfill(2)
