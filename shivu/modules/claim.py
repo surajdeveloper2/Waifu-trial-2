c2902418da50d965b1c7e96c22cea48d35452fb7
surajdeveloper2 <surajget248@gmail.com>
2024-12-26 14:44:31 +0530

    Update claim.py

--- shivu/modules/claim.py
+++ shivu/modules/claim.py
@@ -121,4 +121,9 @@ async def claim(_, message: t.Message):
             ]
             for img_url, caption in zip(img_urls, captions):
                 await message.reply_photo(photo=img_url, caption=caption, parse_mode='HTML')
+                else:
+            await message.reply_text("No characters found for claiming 🤡.")
+    except Exception as e:
+        print(f"An error occurred in claim: {e}")
+
         
