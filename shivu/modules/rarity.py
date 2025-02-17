9c8224be8b374146633e972b1369ce7519d8a15f
surajdeveloper2 <surajget248@gmail.com>
2025-02-17 12:01:10 +0530

    Create rarity.py

---
+++ shivu/modules/rarity.py
@@ -0,0 +1,39 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from itertools import groupby
+import math
+from html import escape 
+import random
+
+from telegram.ext import CommandHandler, CallbackContext, CallbackQueryHandler
+
+from shivu import collection, user_collection, application
+
+RARITY_MAP = {
+    "1": "⚪ Common",
+    "2": "🟣 Normal",
+    "3": "🔵 Medium",
+    "4": "",
+    "5": "💮 Limited",
+    "6": "🔮 Christmas",
+    "7": "🏀 Basketball",
+    "8": "⚽ Football"
+}
+
+selected_rarity = None
+
+async def rarity(update: Update, context: CallbackContext) -> None:
+    global selected_rarity
+    query = update.callback_query
+    data = query.data
+
+    _, rarity_key = data.split(':')
+    selected_rarity = RARITY_MAP[rarity_key]
+
+    await update.message.reply_text(f'Rarity dipilih: {selected_rarity}')
+
+async def harem(update: Update, context: CallbackContext, page=0) -> None:
+    global selected_rarity
+    # ... (isi fungsi harem Anda di sini, dengan modifikasi untuk memeriksa selected_rarity)
+
+RARITY_HANDLER = CommandHandler('rarity', rarity, block=False)
+application.add_handler(RARITY_HANDLER)
