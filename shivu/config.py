63784aaf482990e176bf6f5e965842524862b357
surajdeveloper2 <surajget248@gmail.com>
2025-03-19 10:10:40 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6087651372"
-    sudo_users = "6087651372", "7667987606"
-    GROUP_ID = -1002133191051
-    TOKEN = "7589023976:AAFerTqtgkPEhmNsAU_5XOFtBaTcd7g-3EE"
-    mongo_url = "mongodb+srv://surajgod112:surajgod113@cluster0.v52wo.mongodb.net/"
-    PHOTO_URL = ["https://graph.org/file/40d3df88f6d827ff48af5-9e72ee221223e291ed.jpg", "https://graph.org/file/40d3df88f6d827ff48af5-9e72ee221223e291ed.jpg"]
-    SUPPORT_CHAT = "ntpcraj"
-    UPDATE_CHAT = "ntpcdjk"
-    BOT_USERNAME = "Waifugrls17bot"
-    CHARA_CHANNEL_ID = "-1002300918510"
-    api_id = 29830137
-    api_hash = "4fdb6b13530915e7b513ee4318f109b9"
+    OWNER_ID = "6675050163"
+    sudo_users = "6675050163", "6087651372","7640076990","1420481421","6645960688"
+    GROUP_ID = -1002499806698
+    TOKEN = "8175826981:AAGrz7p7NiE6t1vDIKDBTdbhHrcjB7MGDAo"
+    mongo_url = "mongodb+srv://naruto:hinatababy@cluster0.rqyiyzx.mongodb.net/"
+    PHOTO_URL = ["https://graph.org/file/09e83a1d89aceabd480c5-2afc46a31083fe23f2.jpg", "https://graph.org/file/0aa659508c1add9ae4c86-2b335aa5262b7b64d2.jpg"]
+    SUPPORT_CHAT = "Anime_Circle_Club"
+    UPDATE_CHAT = "Waifu_Chan_Bot_updates"
+    BOT_USERNAME = "@Waifu_Chan_Robot"
+    CHARA_CHANNEL_ID = "-1002640379822"
+    api_id = 28480539
+    api_hash = "6320d9f1bc1f0b72ad66ebdb9d6bfc2c"
 
     
 class Production(Config):
